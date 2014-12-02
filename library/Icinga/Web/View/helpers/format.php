13a698ce15a7a4af89987840f1588575a3d00caa
Alexander Klimov <alexander.klimov@netways.de>
2014-12-02 16:41:55 +0100

    Drop obsolete helpers
    refs #6778

--- library/Icinga/Web/View/helpers/format.php
+++ library/Icinga/Web/View/helpers/format.php
@@ -11,49 +11,6 @@ $this->addHelperFunction('format', function () {
     return Format::getInstance();
 });
 
-/* TODO:
- * drop the following 4 helpers
- * after replacing all their usages w/ dateTimeRenderer
- */
-
-$this->addHelperFunction('timeSince', function ($timestamp) {
-    return sprintf(
-        '<span class="timesince" title="%s">%s</span>',
-        date('Y-m-d H:i:s', $timestamp), // TODO: internationalized format
-        Format::timeSince($timestamp)
-    );
-});
-
-$this->addHelperFunction('prefixedTimeSince', function ($timestamp, $ucfirst = false) {
-    return sprintf(
-        '<span class="timesince" title="%s">%s</span>',
-        date('Y-m-d H:i:s', $timestamp), // TODO: internationalized format
-        Format::prefixedTimeSince($timestamp, $ucfirst)
-    );
-});
-
-$this->addHelperFunction('timeUntil', function ($timestamp) {
-    if (! $timestamp) return '';
-    return sprintf(
-        '<span class="timeuntil" title="%s">%s</span>',
-        date('Y-m-d H:i:s', $timestamp), // TODO: internationalized format
-        Format::timeUntil($timestamp)
-    );
-});
-
-$this->addHelperFunction('prefixedTimeUntil', function ($timestamp, $ucfirst = false) {
-    if (! $timestamp) return '';
-    return sprintf(
-        '<span class="timeuntil" title="%s">%s</span>',
-        date('Y-m-d H:i:s', $timestamp), // TODO: internationalized format
-        Format::prefixedTimeUntil($timestamp, $ucfirst)
-    );
-});
-
-$this->addHelperFunction('dateTimeRenderer', function ($dateTimeOrTimestamp, $future = false) {
-    return DateTimeRenderer::create($dateTimeOrTimestamp, $future);
-});
-
 $this->addHelperFunction('timePointFuture', function ($timestamp) {
     return sprintf(
         '<span class="timeuntil">%s</span>',
