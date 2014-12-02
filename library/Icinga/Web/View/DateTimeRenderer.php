c02fd41327f565d135eba86b87ddc7cc39e0e77d
Alexander Klimov <alexander.klimov@netways.de>
2014-12-02 11:17:51 +0100

    Work only w/ timestamps in DateTimeRenderer
    refs #6778

--- library/Icinga/Web/View/DateTimeRenderer.php
+++ library/Icinga/Web/View/DateTimeRenderer.php
@@ -8,182 +8,101 @@ use DateTime;
 
 class DateTimeRenderer
 {
-    const TYPE_DATETIME = 0;
-    const TYPE_TIME = 1;
-    const TYPE_TIMESPAN = 2;
-
-    const HOUR = 3600;
-
     /**
-     * The present DateTime
+     * The current timestamp
      *
-     * @var DateTime
+     * @var int
      */
     protected $now;
 
     /**
-     * The DateTime tense
+     * The given timestamp
      *
-     * @var bool
-     */
-    protected $future;
-
-    /**
-     * The given DateTime type
-     *
-     * @var integer
-     */
-    protected $type;
-
-    /**
-     * The given DateTime
-     *
-     * @var DateTime
+     * @var int
      */
     protected $dateTime;
 
-    public function __construct($dateTime, $future = false)
+    /**
+     * @param int $dateTime     timestamp accepted by date_timestamp_set()
+     */
+    public function __construct($dateTime)
     {
-        $this->future = $future;
-        $this->now = new DateTime();
-        $this->dateTime = $this->normalize($dateTime);
-        $this->detectType();
+        $this->now = time();
+        $this->dateTime = date_timestamp_get(
+            date_timestamp_set(new DateTime(), $dateTime)
+        );
     }
 
     /**
      * Creates a new DateTimeRenderer
      *
-     * @param DateTime|int $dateTime
-     * @param bool $future
+     * @param int $dateTime
      *
      * @return DateTimeRenderer
      */
-    public static function create($dateTime, $future = false)
+    public static function create($dateTime)
     {
-        return new static($dateTime, $future);
+        return new static($dateTime);
     }
 
     /**
-     * Detects the DateTime context
+     * Render given timestamp (or relative timespan)
+     *
+     * @param bool $future          Future or past?
+     * @param bool $timePoint       Did an event (just) happen once
+     *                              or is a state ongoing?
+     *
+     * @return string
      */
-    protected function detectType()
+    public function render($future = false, $timePoint = false)
     {
-        if ($this->now->format('Y-m-d') === $this->dateTime->format('Y-m-d')) {
-            $this->type = (abs(
-                $this->now->getTimestamp() - $this->dateTime->getTimestamp()
-            ) < self::HOUR) ? self::TYPE_TIMESPAN : self::TYPE_TIME;
+        $diff = abs($this->now - $this->dateTime);
+        $diffParts = array();
+        if ($diff < 60) {
+            $diffParts['s'] = $diff;
+        } elseif ($diff < 3600) {
+            $diffParts['s'] = $diff % 60;
+            $diffParts['i'] = (int) ($diff / 60);
+        } elseif ($diff < 21600) {
+            $diffParts['s'] = $diff % 60;
+            $diff = (int) ($diff / 60);
+            $diffParts['i'] = $diff % 60;
+            $diffParts['h'] = (int) ($diff / 60);
         } else {
-            $this->type = self::TYPE_DATETIME;
+            if ($timePoint) {
+                $grammar = 'at %s';
+            } else {
+                $grammar = $future ? 'until %s' : 'since %s';
+            }
+            return sprintf($grammar, date(
+                (
+                    date('Y-m-d', $this->now) ===
+                    date('Y-m-d', $this->dateTime)
+                ) ? 'H:i:s' : 'Y-m-d H:i:s',
+                $this->dateTime
+            ));
         }
-    }
-
-    /**
-     * Normalizes the given DateTime
-     *
-     * @param DateTime|int $dateTime
-     *
-     * @return DateTime
-     */
-    public static function normalize($dateTime)
-    {
-        if (! ($dt = $dateTime) instanceof DateTime) {
-            $dt = new DateTime();
-            $dt->setTimestamp($dateTime);
+        foreach ($diffParts as $key => $value) {
+            if (0 === $value) {
+                unset($diffParts[$key]);
+            }
         }
-        return $dt;
-    }
-
-    /**
-     * Checks whether DateTime is a date with time
-     *
-     * @return bool
-     */
-    public function isDateTime()
-    {
-        return $this->type === self::TYPE_DATETIME;
-    }
-
-    /**
-     * Checks whether DateTime is a time of the current day
-     *
-     * @return bool
-     */
-    public function isTime()
-    {
-        return $this->type === self::TYPE_TIME;
-    }
-
-    /**
-     * Checks whether DateTime is in a defined interval
-     *
-     * @return bool
-     */
-    public function isTimespan()
-    {
-        return $this->type === self::TYPE_TIMESPAN;
-    }
-
-    /**
-     * Returns the type of the DateTime
-     *
-     * @return mixed
-     */
-    public function getType()
-    {
-        return $this->type;
-    }
-
-    /**
-     * Renders the DateTime on the basis of the type and returns suited text
-     *
-     * @param string $dateTimeText
-     * @param string $timeText
-     * @param string $timespanText
-     *
-     * @return string
-     */
-    public function render($dateTimeText, $timeText, $timespanText)
-    {
-        if ($this->isDateTime()) {
-            return sprintf($dateTimeText, $this);
-        } elseif ($this->isTime()) {
-            return sprintf($timeText, $this);
-        } elseif ($this->isTimespan()) {
-            return sprintf($timespanText, $this);
+        if (0 === count($diffParts)) {
+            $diffParts['s'] = 0;
         }
-
-        return $dateTimeText;
-    }
-
-    /**
-     * Returns a rendered html wrapped text
-     *
-     * @return string
-     */
-    public function __toString()
-    {
-        switch ($this->type) {
-            case self::TYPE_TIME:
-                $format = $this->dateTime->format('H:i:s');
-                break;
-            case self::TYPE_TIMESPAN:
-                $format = $this->dateTime->diff($this->now)->format(t('%im %ss', 'timespan'));
-                break;
-            default:
-                $format = $this->dateTime->format('d.m.Y - H:i:s');
-                break;
-        }
-
-        $css = '';
-        if ($this->type === self::TYPE_TIMESPAN) {
-            $css = $this->future === true ? 'timeuntil' : 'timesince';
-        }
-
-        return sprintf(
-            '<span class="%s" title="%s">%s</span>',
-            $css,
-            $this->dateTime->format('d.m.Y - H:i:s'),
-            $format
+        $formats = array(
+            's' => '%ss',
+            'i' => '%sm',
+            'h' => '%sh'
         );
+        foreach ($diffParts as $key => $value) {
+            $diffParts[$key] = sprintf($formats[$key], $value);
+        }
+        if ($timePoint) {
+            $grammar = $future ? 'in %s' : '%s ago';
+        } else {
+            $grammar = 'for %s';
+        }
+        return sprintf($grammar, implode(' ', array_reverse($diffParts)));
     }
 }
